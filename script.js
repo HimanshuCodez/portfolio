9fa1813a31dae2e47d10374345b05864d679cac7
Himanshu gaur <122437942+Himanshu-gaur-88@users.noreply.github.com>
2023-09-17 20:34:28 +0530

    Update script.js
    added contact section

--- script.js
+++ script.js
@@ -1,177 +1,73 @@
-gsap.registerPlugin(ScrollTrigger);
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
-const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#body"),
-    smooth: true
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the "#body" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#body", {
-    scrollTop(value) {
-        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#body").style.transform ? "transform" : "fixed"
-});
-
-// Typed JS
-var typed = new Typed('#element', {
-    strings: ['Full Stack Developer', 'Coder', 'Musician'],
-    typeSpeed: 90,
-    backSpeed: 70,
-    loop: true
-});
-// Typed JS
-
-// technical skills logo
-gsap.from(".techLogo", {
-    y: 100,
-    stagger: 0.09,
-    delay: 1,
-    duration: .5,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: ".techLogo",
-        scroller: "body",
-    }
-})
-gsap.from("#ts-text", {
-    x: -500,
-    delay: .5,
-    stagger: 1.3,
-    duration: .5,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: "#ts-text",
-        scroller: "body",
-    }
-})
-
-
-
-// GSAP
-gsap.from("#home", {
-    x: -100,
-    delay: .3,
-    duration: 1.5,
-    opacity: 0,
-})
-gsap.from("#about", {
-    x: -100,
-    delay: .3,
-    duration: .5,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: "#about",
-        scroller: "body",
-        scrub: 2,
-        end: "top 30%"
-    }
-})
-gsap.from("#left", {
-    x: -100,
-    delay: .3,
-    duration: .8,
-    opacity: 0
-})
-gsap.from("#demoImg", {
-    x: 150,
-    delay: .3,
-    duration: 1,
-    opacity: 0
-})
-gsap.from("#about-img", {
-    x: -100,
-    delay: .2,
-    duration: .8,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: "#about-img",
-        scroller: "body",
-        scrub: 2,
-        end: "top 30%"
-    }
-})
-gsap.from("#about-me", {
-    x: 150,
-    delay: .2,
-    duration: .8,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: "#about-me",
-        scroller: "body",
-        end: "top 30%",
-        scrub: 2,
-    }
-})
-gsap.from("#proff-skill-text", {
-    x: -100,
-    delay: .2,
-    duration: .8,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: "#proff-skill-text",
-        scroller: "body",
-        end: "top 30%",
-        scrub: 2
-    }
-})
-gsap.from(".l-skills", {
-    x: -100,
-    delay: .2,
-    duration: .8,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: ".l-skills",
-        scroller: "body",
-        scrub: 2,
-        end: "top 30%"
-    }
-})
-
-//Split-text
-const logo = new SplitType('#logo');
-gsap.to('.char', {
-    y: 0,
-    stagger: 0.05,
-    delay: 0.2,
-    duration: .1,
-})
-gsap.from('.line', {
-    y: -200,
-    stagger: 0.05,
-    delay: 0.2,
-    duration: .5,
-})
-
-// Timeline
-let tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".main-c",
-        // markers: true,
-        start: "50% 50%",
-        end: "100% 50%",
-        scrub: 2,
-        pin: true
-    }
-});
-tl.
-    to("#top", {
-        top: "-50%"
-    }, 'a')
-    .to("#bottom", {
-        bottom: "-50%"
-    }, 'a')
-    .to("#top-h", {
-        bottom: "30%"
-    }, 'a')
-    .to("#bottom-h", {
-        bottom: "-70%"
-    }, 'a')
